5015271c3915d936f01625001cb414cebbcf1344
tuchtham <touch0869@gmail.com>
2023-10-19 15:16:50 +0700

    feat: Assignment Week 10

--- vaccine-book-app/src/app/booking/page.tsx
+++ vaccine-book-app/src/app/booking/page.tsx
@@ -1,9 +1,35 @@
 import BookingForm from "@/components/BookingForm";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import getUserProfile from "@/libs/getUserProfile";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+  if (!session || !session.user.token) return null;
+
+  const profile = await getUserProfile(session.user.token);
+  var createdAt = new Date(profile.data.createdAt);
+
   return (
     <main>
       <h1 className="text-center mt-20 text-9xl"></h1>
+      <div className="text-2xl">{profile.data.name}</div>
+      <table className="table-auto border-separate border-spacing-2">
+        <tbody>
+          <tr>
+            <td>Email</td>
+            <td>{profile.data.email}</td>
+          </tr>
+          <tr>
+            <td>Tel</td>
+            <td>{profile.data.tel}</td>
+          </tr>
+          <tr>
+            <td>Member Since</td>
+            <td>{createdAt.toString()}</td>
+          </tr>
+        </tbody>
+      </table>
       <div className="content-center m-20">
         <BookingForm></BookingForm>
       </div>
--- vaccine-book-app/src/components/Banner.tsx
+++ vaccine-book-app/src/components/Banner.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { useState } from "react";
 import styles from "./banner.module.css";
 import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
 
 export default function Banner() {
   const covers = [
@@ -13,6 +14,9 @@ export default function Banner() {
   ];
   const [index, setIndex] = useState(0);
   const router = useRouter();
+  const { data: session } = useSession();
+  console.log(session?.user.token);
+
   return (
     <div className={styles.banner} onClick={() => setIndex((index + 1) % 4)}>
       <Image
@@ -26,7 +30,18 @@ export default function Banner() {
         <h1 className="text-4xl font-medium">Your Vaccine Boi</h1>
         <h3 className="text-xl font-serif">Trust me bro</h3>
       </div>
-      <button className="bg-white text-cyan-600 border border-cyan-600 font-semibold py-2 px-2 m-2 rounded z-30 absolute bottom-0 right-0 hover:bg-cyan-600 hover:text-white hover:border-transparent" onClick={(e)=>{e.stopPropagation(); router.push('/hospital')}}>
+      {session ? (
+        <div className="z-20 absolute top-30 right-10 font-semibold text-cyan-100 text-xl">
+          Hello {session.user?.name}
+        </div>
+      ) : null}
+      <button
+        className="bg-white text-cyan-600 border border-cyan-600 font-semibold py-2 px-2 m-2 rounded z-30 absolute bottom-0 right-0 hover:bg-cyan-600 hover:text-white hover:border-transparent"
+        onClick={(e) => {
+          e.stopPropagation();
+          router.push("/hospital");
+        }}
+      >
         Select Your Vaccine NOW
       </button>
     </div>
